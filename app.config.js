08fa3985f467f3db83a448a74056ea4450ef6130
Jayesh Patil <jayeshrpatil003@gmail.com>
2025-07-03 09:26:37 +0530

    build problem solve

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
   ...config,
   name: "BabyFlix",
   slug: "babyflix",
-  version: "1.0.3",
+  version: "1.1.3",
   scheme: "babyflix",
   extra: {
     eas: {
@@ -64,6 +64,7 @@ export default ({ config }) => ({
   },
   plugins: [
     "expo-router",
+    "expo-web-browser",
     [
       "expo-image-picker",
       {
